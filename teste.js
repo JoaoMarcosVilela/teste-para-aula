fc942fd9c094fe1154cf1be328ee4ef9dbb5924b
JoaoMarcosVilela <joaosemp12345@gmail.com>
2024-11-05 14:39:51 -0300

    Enviando teste para validar

--- teste.js
+++ teste.js
@@ -1,10 +1,12 @@
 var contador = 0;
 
-function simulacao(){
-
-    if(contador == 5){
-        return "Testes finalizados";
+function rodarTeste() {
+    if (contador === 5) {
+        return console.log("Testes finalizados");
     }
-    contador+=1;
-    return `echo rodando ${contador}`;
+    contador += 1;
+    console.log(`Rodando teste ${contador}`);
+    setTimeout(rodarTeste, 1000);  // Chama a função novamente após 1 segundo
 }
+
+rodarTeste();  // Chama a função inicialmente
